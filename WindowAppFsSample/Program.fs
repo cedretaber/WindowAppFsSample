6cace6282bb187e8a5398aec4a29cbd60a062e0c
cedretaber <cedretaber@gmail.com>
2017-08-13 01:45:01 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowAppFsSample/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Markup
+
+open FsXaml
+
+type MainWindow = XAML<"MainWindow.xaml">
+
+let force_int_of_string : string -> int =
+  fun str -> match Int32.TryParse str with _, n -> n
+ 
+[<STAThread>]
+[<EntryPoint>]
+let main argv =
+  let mainWindow = MainWindow () in
+
+  let f = fun _ ->
+    let x = force_int_of_string mainWindow.numberX.Text in
+    let y = force_int_of_string mainWindow.numberY.Text in
+    mainWindow.numberZ.Text <- (x + y).ToString ()
+  
+  mainWindow.numberX.TextChanged |> Event.add f
+  mainWindow.numberY.TextChanged |> Event.add f
+
+  (new Application()).Run mainWindow
